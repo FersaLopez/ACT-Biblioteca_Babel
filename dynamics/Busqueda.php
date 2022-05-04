319d2faee9e4fe7032224a17992650a5a4f7f9ba
Fersa <321172974@alumno.enp.unam.mx>
2022-05-03 19:20:10 -0500

    Forma Normal lista, en progreso con las demas (pendiente el formato)

--- dynamics/Busqueda.php
+++ dynamics/Busqueda.php
@@ -4,30 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Busqueda Libro</title>
 </head>
 <body>
     <?php
         $buscar = (isset($_POST["Buscar"]) && $_POST["Buscar"] != "")? $_POST['Buscar'] : "no especifico";
         $zona = (isset($_POST['Zona']) && $_POST["Zona"] != "")? $_POST['Zona'] : "no especifico";
         $modo = (isset($_POST['MODO']) && $_POST["MODO"] != "")? $_POST['MODO'] : "no especifico";
-
-        echo 'Busqueda: ';
-        echo $buscar;
-        echo '<br>';
-        if($zona=='value1')
-        {
-            echo 'Zona Horaria: New York';
-        }
-        if($zona=='value2')
-        {
-            echo 'Zona Horaria: Mexico City';
-        }
-        if($zona=='value3')
-        {
-            echo 'Zona Horaria: Berlín';
-        }
-        echo '<br>';
+        
         if($modo=='value4')
         {
             echo 'Modo: Oración';
@@ -41,47 +25,75 @@
             echo 'Modo: Palabras';
         }
         echo '<br><br>';
-        
-        
-        if($modo=='value5')
-        {
-            echo "
-                <table border='1'>
-                    
-                    <tbody>
-                        <tr>
-                            <td align='center'>";
-                            
-                                echo 'Libro ';
-                                echo rand(4000,6000);
-                            echo "</td>
-                        </tr>
-                        <tr>
-                            <td>";
-
-                                $long_texto=rand(300,500); 
+                
+        echo "
+            <table border='1'>                    <!--Poner formato agradable a la tabla-->
+                <thead>
+                    <tr>
+                        <th>";   
+                            $long_identificador=rand();             //Poner un texto mas aleatorio con CARACTERES
+                            echo "<br>Libro $long_identificador <br><br>";
+                    echo "</th>
+                    </tr>
+                </thead>
+                <tbody>                                                                                                                                    
+                    <tr>                    
+                        <td>";
+                            if($modo=='Normal')
+                            {                                                                
+                                $long_texto=rand(300,500);                                 
+                                //$cadena= "Hola soy un aguacate"; 
+                                $insertar = rand(0, $long_texto);
 
                                 for($i=0; $i<$long_texto; $i++)
+                                {
+                                    $long_palabra=rand(4,10);
+                                    
+                                    for($p=0; $p<$long_palabra; $p++)
                                     {
-                                        $long_palabra=rand(4,10);
-                                
-                                        for($p=0; $p<$long_palabra; $p++)
-                                        {
-                                            $ascii=rand(97, 122); 
-                                            $letra= chr($ascii); 
-                                            echo "$letra"; 
-                                        }
-                                        echo " ";  
+                                        $ascii=rand(97, 122); 
+                                        $letra= chr($ascii); 
+                                        echo "$letra"; 
                                     }
-                                    echo $buscar;
-                            echo"
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            ";
+                                    echo " "; 
+                                    if($i==$insertar){
+                                        echo "<strong>$buscar </strong>";
+                                    }
+                                }
+                
+                            }
+                            else if($modo=='Orden')
+                            {
+                                
+                            }
+                            else
+                            {
+                                
+                            }
+                            
+                        echo"
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            ";        
+            if($zona=='value1')
+            {
+                echo 'Zona Horaria: New York';
+            }
+            if($zona=='value2')
+            {
+                echo 'Zona Horaria: Mexico City';
+            }
+            if($zona=='value3')
+            {
+                echo 'Zona Horaria: Berlín';
+            }
             echo '<br>';
-        }
+            echo '<br>';                                
+            echo 'Busqueda: ';
+            echo $buscar;
+            echo '<br>';
     ?>
 </body>
 </html>
